6a101e51f21be4bed45f28e89e1a1aeac94d3389
Fiskoff <fiskoff2015@yandex.ru>
2025-07-23 22:57:10 +0700

    Created func get_user

--- telegram_crm/app/repository/user_repository.py
+++ telegram_crm/app/repository/user_repository.py
@@ -1,3 +1,4 @@
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models.user_model import User
@@ -7,7 +8,10 @@ class UserRepository:
     def __init__(self, async_session: AsyncSession):
         self.session = async_session
 
-
-    async def create_user(self, user_id:int, username:str, role:str, position:str):
-        self.session.add(User(telegram_id=user_id, full_name=username, role=role, position=position))
+    async def create_user(self, telegram_id:int, full_name:str, role:str, position:str):
+        self.session.add(User(telegram_id=telegram_id, full_name=full_name, role=role, position=position))
         await self.session.commit()
+
+    async def get_user(self, input_user_id) -> User | None:
+        result = await self.session.execute(select(User).where(User.telegram_id == input_user_id))
+        return result.scalars().first()
