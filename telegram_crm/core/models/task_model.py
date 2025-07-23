eb9a1c2cfb52bd586c54c0ec7da86baaaa334246
Fiskoff <fiskoff2015@yandex.ru>
2025-07-23 22:01:56 +0700

    Add relation between models and fix models

--- telegram_crm/core/models/task_model.py
+++ telegram_crm/core/models/task_model.py
@@ -4,6 +4,7 @@ from sqlalchemy import Integer, String, Text, DateTime, Enum, func, ForeignKey, 
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from telegram_crm.core.models.base_model import BaseModel, TaskStatus
+from telegram_crm.core.models.user_model import User
 
 
 class Task(BaseModel):
@@ -13,11 +14,21 @@ class Task(BaseModel):
     description: Mapped[str] = mapped_column(Text, nullable=False)
     deadline: Mapped[datetime] = mapped_column(DateTime, nullable=False)
     status: Mapped[TaskStatus] = mapped_column(Enum(TaskStatus), default=TaskStatus.ACTIVE, nullable=False)
-    created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now(), nullable=True)
-    completed_at: Mapped[datetime] = mapped_column(DateTime, nullable=True)
-    comment: Mapped[str] = mapped_column(Text, nullable=True)
-    photo_url: Mapped[str] = mapped_column(String, nullable=True)
+    created_at: Mapped[datetime] = mapped_column(DateTime, server_default=func.now(), nullable=False)
+    completed_at: Mapped[datetime | None] = mapped_column(DateTime, nullable=True)
+    comment: Mapped[str | None] = mapped_column(Text, nullable=True)
+    photo_url: Mapped[str | None] = mapped_column(String, nullable=True)
 
-    executor_id = mapped_column(BigInteger, ForeignKey("users.id"))
-    manager_id = mapped_column(BigInteger, ForeignKey("users.id"))
+    executor_id: Mapped[BigInteger | None] = mapped_column(BigInteger, ForeignKey("users.telegram_id"), nullable=True)
+    manager_id: Mapped[BigInteger] = mapped_column(BigInteger, ForeignKey("users.telegram_id"), nullable=False)
 
+    executor: Mapped["User | None"] = relationship(
+        "User",
+        back_populates="executed_tasks",
+        foreign_keys=[executor_id]
+    )
+    manager: Mapped["User"] = relationship(
+        "User",
+        back_populates="managed_tasks",
+        foreign_keys=[manager_id]
+    )
--- telegram_crm/core/models/user_model.py
+++ telegram_crm/core/models/user_model.py
@@ -1,5 +1,5 @@
 from sqlalchemy import String, BigInteger, Enum
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from telegram_crm.core.models.base_model import BaseModel, UserRole
 
@@ -10,4 +10,16 @@ class User(BaseModel):
     telegram_id: Mapped[BigInteger] = mapped_column(BigInteger, primary_key=True, index=True)
     full_name: Mapped[str] = mapped_column(String(100), nullable=False)
     role: Mapped[UserRole] = mapped_column(Enum(UserRole), default=UserRole.STAFF, index=True, nullable=False)
-    position: Mapped[str] = mapped_column(String(100), nullable=False)
+    position: Mapped[str] = mapped_column(String(100), nullable=True)
+
+
+    managed_tasks = relationship(
+        "Task",
+        back_populates="manager",
+        foreign_keys="Task.manager_id"
+    )
+    executed_tasks = relationship(
+        "Task",
+        back_populates="executor",
+        foreign_keys="Task.executor_id"
+    )
