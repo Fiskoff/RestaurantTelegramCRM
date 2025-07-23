1585facd9ada6a55798302e92119d41610b24d2d
Fiskoff <fiskoff2015@yandex.ru>
2025-07-24 00:36:46 +0700

    Fix UserService

--- telegram_crm/app/services/user_service.py
+++ telegram_crm/app/services/user_service.py
@@ -1,15 +1,21 @@
 from core.db_helper import db_helper
 from app.repository.user_repository import UserRepository
+from core.models.base_model import UserRole
 
 
 class UserService:
+    @staticmethod
+    async def get_user_by_telegram_id(telegram_id: int):
+        async with db_helper.session_factory() as session:
+            user_repository = UserRepository(session)
+            return await user_repository.get_user(telegram_id)
 
     @staticmethod
-    async def create_new_user(telegram_id:int, full_name:str, role:str, position:str) -> dict:
+    async def create_new_user(telegram_id: int, full_name: str, role: UserRole, position: str) -> dict:
         async with db_helper.session_factory() as session:
             user_repository = UserRepository(session)
             user = await user_repository.get_user(telegram_id)
             if user is None:
                 await user_repository.create_user(telegram_id, full_name, role, position)
-                return {"success": True, "message": "User created successfully"}
-            return {"success": False, "message": "Such a user already exists"}
+                return {"success": True, "message": "Пользователь успешно создан"}
+            return {"success": False, "message": "Пользователь уже существует"}
