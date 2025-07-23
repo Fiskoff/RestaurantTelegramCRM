6b9a28d885cd1a96dcbe1ef1c88e8241328c3bf7
Fiskoff <fiskoff2015@yandex.ru>
2025-07-23 21:03:28 +0700

    Created enum classes

--- telegram_crm/core/models/base_model.py
+++ telegram_crm/core/models/base_model.py
@@ -1,5 +1,18 @@
+from enum import Enum
+
 from sqlalchemy.orm import DeclarativeBase
 
 
 class BaseModel(DeclarativeBase):
     pass
+
+
+class UserRole(Enum):
+    MANAGER = "manager"
+    STAFF = "staff"
+
+
+class TaskStatus(Enum):
+    ACTIVE = "active"
+    COMPLETED = "completed"
+    OVERDUE = "overdue"
