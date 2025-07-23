9908888ffe50dcb5c53a2508d1a901fb27306a30
Fiskoff <fiskoff2015@yandex.ru>
2025-07-24 00:39:22 +0700

    Connected register router and init memory storage

--- telegram_crm/main.py
+++ telegram_crm/main.py
@@ -2,20 +2,16 @@ import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher
-from aiogram.filters import CommandStart
-from aiogram.types import Message
+from aiogram.fsm.storage.memory import MemoryStorage
 
 from core.config import settings
+from app.handlers.registration_handler import register_router
 
 
 bot = Bot(settings.tg.token)
-dispatcher = Dispatcher()
-
-
-@dispatcher.message(CommandStart())
-async def cmd_start(message: Message):
-    await message.answer(f"Hello, {message.from_user.first_name}!")
-
+storage = MemoryStorage()
+dispatcher = Dispatcher(storage=storage)
+dispatcher.include_router(register_router)
 
 async def main():
     await dispatcher.start_polling(bot)
