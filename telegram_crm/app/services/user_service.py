a2541e611a56e233a097e14a9e856cdfc064653b
Fiskoff <fiskoff2015@yandex.ru>
2025-07-23 23:06:53 +0700

    Created class UserService and func create_new_user

---
+++ telegram_crm/app/services/user_service.py
@@ -0,0 +1,15 @@
+from core.db_helper import db_helper
+from app.repository.user_repository import UserRepository
+
+
+class UserService:
+
+    @staticmethod
+    async def create_new_user(telegram_id:int,full_name:str, role:str, position:str) -> dict:
+        async with db_helper.session_factory() as session:
+            user_repository = UserRepository(session)
+            user = await user_repository.get_user(telegram_id)
+            if user is None:
+                await user_repository.create_user(telegram_id, full_name, role, position)
+                return {"success": True, "message": "User created successfully"}
+            return {"success": False, "message": "Such a user already exists"}
