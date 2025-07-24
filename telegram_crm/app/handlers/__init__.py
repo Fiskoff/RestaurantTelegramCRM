01bb26f9a8f288b789631036903877510835a528
Fiskoff <fiskoff2015@yandex.ru>
2025-07-25 00:00:54 +0700

    Created all_router and connection to dispatcher

---
+++ telegram_crm/app/handlers/__init__.py
@@ -0,0 +1,10 @@
+from aiogram import Router
+
+from app.handlers.registration_handler import register_router
+from app.handlers.manager_handlers import manager_router
+
+
+all_routers = Router()
+
+all_routers.include_router(register_router)
+all_routers.include_router(manager_router)
--- telegram_crm/main.py
+++ telegram_crm/main.py
@@ -5,13 +5,13 @@ from aiogram import Bot, Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 
 from core.config import settings
-from app.handlers.registration_handler import register_router
+from app.handlers import all_routers
 
 
 bot = Bot(settings.tg.token)
 storage = MemoryStorage()
 dispatcher = Dispatcher(storage=storage)
-dispatcher.include_router(register_router)
+dispatcher.include_router(all_routers)
 
 async def main():
     await dispatcher.start_polling(bot)
