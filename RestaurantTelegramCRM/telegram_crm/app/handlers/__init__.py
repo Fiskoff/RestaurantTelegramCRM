d573842fcfa5723cc85e87d52a150cfde128456f
Fiskoff <fiskoff2015@yandex.ru>
2025-07-29 18:06:42 +0700

    Connected task router

--- RestaurantTelegramCRM/telegram_crm/app/handlers/__init__.py
+++ RestaurantTelegramCRM/telegram_crm/app/handlers/__init__.py
@@ -2,9 +2,10 @@ from aiogram import Router
 
 from app.handlers.registration_handler import register_router
 from app.handlers.manager_handlers import manager_router
-
+from app.handlers.task_handlers import task_router
 
 all_routers = Router()
 
 all_routers.include_router(register_router)
 all_routers.include_router(manager_router)
+all_routers.include_router(task_router)
