435e0cc503a30c1b00ec753c525b4a37653cc15b
Fiskoff <fiskoff2015@yandex.ru>
2025-07-29 23:20:15 +0700

    rename func

--- RestaurantTelegramCRM/telegram_crm/app/keyboards/select_all_task_keyboard.py
+++ RestaurantTelegramCRM/telegram_crm/app/keyboards/select_all_task_keyboard.py
@@ -3,7 +3,7 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from app.services.task_service import TaskService
 
 
-async def create_employee_selection_keyboard(telegram_id: int) -> InlineKeyboardMarkup:
+async def select_all_tasks_keyboard(telegram_id: int) -> InlineKeyboardMarkup:
     tasks = await TaskService.get_all_task(telegram_id)
     buttons = []
     for task in tasks:
